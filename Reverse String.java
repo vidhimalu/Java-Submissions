ac00d53d69557ee5a03929f5ecfd69317684bf82
vidhimalu <vidhimalu0@gmail.com>
2026-04-08 22:40:36 +0530

    Create Reverse String.java

---
+++ Reverse String.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        sol.reverseString(s);
+
+        System.out.println(s); 
+    }
+}
