6bf5a68916e594f8788da0e307822b14652cc254
vidhimalu <vidhimalu0@gmail.com>
2026-05-10 21:21:59 +0530

    Create Maximum Number of Jumps to reach the last index.java

---
+++ Maximum Number of Jumps to reach the last index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == -1) continue;
+            
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(nums[j] - nums[i]) <= target) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+        
+        return dp[n - 1];
+    }
+}
